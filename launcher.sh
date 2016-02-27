92d78a8fac5c3adedd83940fdd1f188eed833fd6
Antoine Webanck <antoine.webanck@gmail.com>
2016-02-27 23:50:07 +0100

    The mounting problems encountered after reboot and restart of the container seems to be solved so no reason to continue making new containers each time.

--- launcher.sh
+++ launcher.sh
@@ -2,17 +2,20 @@
 
 CONTAINER_NAME=vaporized_wine
 
-echo 'Trying to run new data container.'
-#The container will be destroyed when left to avoid volume mounting problems, especially the hold of the Xauthority and pulseaudio files by non destroyed Docker volumes causing OS and Docker failures after reboot.
-sudo docker run -ti --rm \
-		-e DISPLAY \
-		-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-		-v ~/.Xauthority:/home/wine/.Xauthority \
-		-v /dev/snd:/dev/snd --privileged \
-		-v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native \
-		-v `pwd`/shared_directory:/home/wine/shared_directory \
-		--net=host \
-		--name $CONTAINER_NAME \
-		webanck/docker-wine-steam \
-|| \
-echo 'Container already exists, it need to be erased.'
+( \
+	echo 'Trying to run a new data container.' && \
+	sudo docker run -ti \
+			-e DISPLAY \
+			-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+			-v ~/.Xauthority:/home/wine/.Xauthority \
+			-v /dev/snd:/dev/snd --privileged \
+			-v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native \
+			-v `pwd`/shared_directory:/home/wine/shared_directory \
+			--net=host \
+			--name "$CONTAINER_NAME" \
+			webanck/docker-wine-steam \
+	2>/dev/null \
+) || ( \
+	echo 'The container already exists, relaunching the old old one.' && \
+	sudo docker start -ai "$CONTAINER_NAME" \
+)
