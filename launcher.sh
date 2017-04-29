9bf2a101e0651bba6a95552834d0d6b786477eec
Zhenbo Li <litimetal@gmail.com>
2017-04-29 09:59:04 +0800

    Add command line arguments.
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 
 CONTAINER_NAME=vaporized_wine
+IMAGE=webanck/docker-wine-steam
+REMOVE_OLD_CONTAINER=0
+
+while [[ $@ ]]
+do
+key="$1"
+case $key in
+    -c|--container_name)
+    CONTAINER_NAME="$2"
+    shift # past argument
+    ;;
+    -i|--image)
+    IMAGE="$2"
+    shift # past argument
+    ;;
+    -r|--remove)
+    REMOVE_OLD_CONTAINER=1
+    ;;
+    *)
+    echo "Unsolved parameter:" $key
+    ;;
+esac
+shift # past argument or value
+done
+
+if [[ $REMOVE_OLD_CONTAINER = 1 ]]; then
+    sudo docker rm $CONTAINER_NAME ||\
+    echo $CONTAINER_NAME "not found, skip removing phase"
+fi
 
 ( \
 	echo 'Trying to run a new data container.' && \
@@ -13,7 +42,7 @@ CONTAINER_NAME=vaporized_wine
 			-v `pwd`/shared_directory:/home/wine/shared_directory \
 			--net=host \
 			--name "$CONTAINER_NAME" \
-			webanck/docker-wine-steam \
+			$IMAGE \
 	2>/dev/null \
 ) || ( \
 	echo 'The container already exists, relaunching the old old one.' && \
